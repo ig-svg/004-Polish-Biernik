17cf837a2bd929b2f5b056276a1dd4dd30d3d2ab
Illya Glukhanyuk <ig@bimhelp.me>
2026-02-03 17:53:25 +0000

    chore: universal migration to vite and tailwind fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App />
